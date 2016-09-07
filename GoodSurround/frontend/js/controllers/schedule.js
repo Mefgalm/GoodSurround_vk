5863d25cce275ea80ef869a05b05b68963759b43
DmitryShashkov <dmitry.the.wise@gmail.com>
2016-09-07 22:13:37 +0300

    Sending selected backwards

--- GoodSurround/frontend/js/controllers/schedule.js
+++ GoodSurround/frontend/js/controllers/schedule.js
@@ -17,8 +17,8 @@ angular.module('GoodSurround').controller('ScheduleController', ['$scope', '$uib
                 controller: 'PeopleSearchController'
             });
 
-            modalInstance.result.then(function success (message) {
-                console.log(message);
+            modalInstance.result.then(function success (selectedUsers) {
+                $scope.users = $scope.users.concat(selectedUsers);
             }, function failure () {
                 console.log('Modal dismissed at: ' + new Date());
             });
--- GoodSurround/frontend/js/controllers/search-people.js
+++ GoodSurround/frontend/js/controllers/search-people.js
@@ -1,19 +1,42 @@
 angular.module('GoodSurround').controller('PeopleSearchController', [ '$uibModalInstance', '$scope', 'Users',
 function ($uibModalInstance, $scope, Users) {
     $scope.ok = function () {
-        $uibModalInstance.close('ok');
+        $uibModalInstance.close($scope.selectedUsers);
     };
 
     $scope.searchQuery = '';
     $scope.displayResultsThreshold = 3;
     $scope.searchResults = [];
+    $scope.selectedUsers = [];
 
+    // this'd better be ng-change handler
     $scope.$watch('searchQuery', function (newValue, oldValue) {
         console.log(newValue, oldValue);
         Users.find(newValue).then(function (results) {
             // todo: implement infinite scroll
-            $scope.searchResults = results;
+            $scope.searchResults = results.map(function (user) {
+                user.isSelected = false;
+                return user;
+            });
+            $scope.selectedUsers = _.intersection($scope.selectedUsers, $scope.searchResults)
+                .map(function (user) {
+                    user.isSelected = true;
+                    return user;
+                });
+            // to clear users when search results are not displayed
+            if ($scope.searchQuery === '') {
+                $scope.selectedUsers = [];
+            }
             $scope.$apply();
         });
     });
+
+    $scope.toggleSelection = function (user) {
+        if (_.contains($scope.selectedUsers, user)) {
+            $scope.selectedUsers = _.without($scope.selectedUsers, user);
+        } else {
+            $scope.selectedUsers.push(user);
+        }
+        user.isSelected = !user.isSelected;
+    };
 }]);
